8e83d728411891e08b168e5da804eb81fd72ceb0
anekobtw <krolexonder@gmail.com>
2024-09-28 14:46:41 +0400

    gui changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import customtkinter as ctk
 from icrawler.builtin import BingImageCrawler, GoogleImageCrawler
 from PIL import Image
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,38 +20,36 @@ class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Images Downloader")
-        self.draw_sidebar()
-        self.draw_mainbar()
+        self.draw_infobar()
+        self.draw_settingsbar()
 
-    def draw_sidebar(self) -> None:
-        sidebar_frame = ctk.CTkFrame(self, width=140)
-        sidebar_frame.grid(row=0, column=0, rowspan=4, pady=20, padx=20, sticky="nsew")
-        sidebar_frame.grid_rowconfigure(4, weight=1)
+    def draw_infobar(self) -> None:
+        sidebar = ctk.CTkFrame(self, width=140)
+        sidebar.grid(row=0, column=0, pady=20, padx=20, sticky="nsew")
 
-        ctk.CTkLabel(sidebar_frame, text=f"Images Downloader {__version__}", font=ctk.CTkFont(size=20, weight="bold")).grid(row=0, column=0, padx=20, pady=(20, 0))
-        ctk.CTkLabel(sidebar_frame, text="© anekobtw, 2024").grid(row=1, column=0, padx=20, pady=(0, 20))
-        ctk.CTkLabel(sidebar_frame, text="Appearance Mode:").grid(row=5, column=0, padx=20)
+        ctk.CTkLabel(sidebar, text=f"Images Downloader {__version__}", font=ctk.CTkFont(size=20, weight="bold")).grid(padx=20, pady=(10, 0))
+        ctk.CTkLabel(sidebar, text="© anekobtw, 2024").grid(padx=10, pady=(5, 10))
 
-        self.appearance_mode_optionmenu = ctk.CTkOptionMenu(sidebar_frame, values=["Light", "Dark", "System"], command=lambda x: ctk.set_appearance_mode(x))
-        self.appearance_mode_optionmenu.set("System")
-        self.appearance_mode_optionmenu.grid(row=6, column=0, padx=20, pady=(0, 10))
+        self.query_entry = ctk.CTkEntry(sidebar, placeholder_text="Query")
+        self.query_entry.grid(padx=10, pady=(20, 0))
+        ctk.CTkButton(sidebar, text="Download", command=self.download).grid(padx=10, pady=(10, 20))
 
-    def draw_mainbar(self) -> None:
-        mainbar_frame = ctk.CTkFrame(self, width=140)
-        mainbar_frame.grid(row=0, column=1, rowspan=4, pady=20, padx=20, sticky="nsew")
-        self.query_entry = ctk.CTkEntry(mainbar_frame, placeholder_text="Query")
-        self.query_entry.grid(row=0, column=0, padx=20, pady=10)
-        self.filter_checkbox = ctk.CTkCheckBox(mainbar_frame, text="Filter", command=lambda: [state.configure(state=["disabled", "normal"][self.filter_checkbox.get()], placeholder_text=text) for state, text in [(self.res_entry1, "height"), (self.res_entry2, "width")]])
+        self.appearance = ctk.CTkOptionMenu(sidebar, values=["Light", "Dark", "System"], command=lambda x: ctk.set_appearance_mode(x))
+        self.appearance.set("System")
+        self.appearance.grid(padx=10, pady=10)
+
+    def draw_settingsbar(self) -> None:
+        settingsbar = ctk.CTkFrame(self)
+        settingsbar.grid(row=0, column=1, pady=20, padx=20, sticky="nsew")
+
+        self.filter_checkbox = ctk.CTkCheckBox(settingsbar, text="Filter", command=lambda: [state.configure(state=["disabled", "normal"][self.filter_checkbox.get()], placeholder_text=text) for state, text in [(self.res_entry1, "height"), (self.res_entry2, "width")]])
         self.filter_checkbox.grid(row=1, column=0, padx=20, pady=10)
-
-        res_frame = ctk.CTkFrame(mainbar_frame, fg_color="transparent")
+        res_frame = ctk.CTkFrame(settingsbar, fg_color="transparent")
         res_frame.grid(row=2, column=0, pady=10, padx=20, sticky="w")
         self.res_entry1 = ctk.CTkEntry(res_frame, state="disabled", width=50)
         self.res_entry1.pack(side="left", padx=(0, 40))
         self.res_entry2 = ctk.CTkEntry(res_frame, state="disabled", width=50)
         self.res_entry2.pack(side="right")
-
-        ctk.CTkButton(mainbar_frame, text="Download", command=self.download).grid(row=3, column=0, padx=20, pady=5)
 
     def download(self) -> None:
         self.download_images(self.query_entry.get())
