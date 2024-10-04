49a704585326cea400c28bf39598f8744fa63f2e
anekobtw <krolexonder@gmail.com>
2024-10-04 17:54:12 +0400

    v2.0.0?

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import os
 
 import customtkinter as ctk
 
-from crawlers import download_images
+from crawlers import download_images, filter_images
 
 # Basic configuring
-__version__ = "1.2.1"
+__version__ = "2.0.0"
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 ctk.set_default_color_theme("theme.json")
 PADX = 20
@@ -17,62 +17,65 @@ window = ctk.CTk()
 window.title = "Images Downloader"
 
 infobar = ctk.CTkFrame(window)
-infobar.grid(row=0, column=0, padx=PADX, pady=20, sticky="nsew")
+infobar.grid(row=0, column=0, padx=PADX, pady=20, columnspan=2)
 
-settingsbar = ctk.CTkFrame(window)
-settingsbar.grid(row=0, column=1, padx=PADX, pady=20, sticky="ew")
+downloadbar = ctk.CTkFrame(window)
+downloadbar.grid(row=1, column=0, padx=PADX, pady=10, sticky="nsew")
 
-gp_frame = ctk.CTkFrame(settingsbar, fg_color="transparent")
-gp_frame.grid(row=1, pady=10, padx=PADX, sticky="nsew")
-
-res_frame = ctk.CTkFrame(settingsbar, fg_color="transparent")
-res_frame.grid(row=3, pady=10, padx=PADX, sticky="nsew")
-
-font = ctk.CTkFont(size=20, weight="bold")
+filterbar = ctk.CTkFrame(window)
+filterbar.grid(row=1, column=1, padx=PADX, pady=10, sticky="nsew")
 
 
 # Infobar
-ctk.CTkLabel(infobar, text=f"Images Downloader {__version__}", font=font).grid(padx=PADX, pady=(10, 0))
+ctk.CTkLabel(infobar, text=f"Images Downloader {__version__}", font=ctk.CTkFont(size=20, weight="bold")).grid(padx=PADX, pady=(10, 0))
 ctk.CTkLabel(infobar, text="© anekobtw, 2024\ntheme by avalon60").grid(padx=PADX, pady=(5, 10))
 
-query_entry = ctk.CTkEntry(infobar, placeholder_text="Query")
-query_entry.grid(padx=10, pady=(20, 0))
+
+# Download bar
+query_entry = ctk.CTkEntry(downloadbar, placeholder_text="Query")
+query_entry.grid(row=0, column=0, padx=10, pady=(10, 0), columnspan=2)
+
+google_checkbox = ctk.CTkCheckBox(downloadbar, text="Google", corner_radius=36)
+google_checkbox.grid(row=1, column=0, sticky="w", padx=PADX, pady=(10, 20))
+google_checkbox.select()
+
+pinterest_checkbox = ctk.CTkCheckBox(downloadbar, text="Pinterest", width=50, corner_radius=36)
+pinterest_checkbox.grid(row=1, column=1, sticky="w", padx=PADX, pady=(10, 20))
+pinterest_checkbox.select()
 
 ctk.CTkButton(
-    infobar,
+    downloadbar,
     text="Download",
     command=lambda: download_images(
         query=query_entry.get(),
         google=google_checkbox.get(),
         pinterest=pinterest_checkbox.get(),
+    ),
+).grid(row=2, column=0, pady=10, columnspan=2)
+
+ctk.CTkButton(downloadbar, text="Open Downloads Folder", command=lambda: os.startfile("downloads")).grid(row=3, column=0, pady=(0, 10), columnspan=2)
+
+
+# Filter bar
+height_entry = ctk.CTkEntry(filterbar, width=50, placeholder_text="height")
+height_entry.grid(row=0, column=0, sticky="w", padx=PADX, pady=10)
+
+width_entry = ctk.CTkEntry(filterbar, width=50, placeholder_text="width")
+width_entry.grid(row=0, column=1, sticky="w", padx=PADX, pady=10)
+
+rembg_checkbox = ctk.CTkCheckBox(filterbar, text="Remove background", width=50, corner_radius=36)
+rembg_checkbox.grid(row=1, column=0, sticky="w", padx=PADX, pady=(5, 10), columnspan=2)
+
+ctk.CTkButton(
+    filterbar,
+    text="Filter",
+    command=lambda: filter_images(
         height=height_entry.get(),
         width=width_entry.get(),
         remove_bg=rembg_checkbox.get(),
     ),
-).grid(padx=10, pady=(10, 20))
+).grid(row=2, column=0, pady=(53, 0), columnspan=2)
 
-ctk.CTkButton(infobar, text="Open Downloads Folder", command=lambda: os.startfile("downloads")).grid(pady=(5, 13))
-
-
-# Settings bar
-ctk.CTkLabel(settingsbar, text="Settings", font=font).grid(row=0, pady=(10, 0))
-
-google_checkbox = ctk.CTkCheckBox(gp_frame, text="Google", corner_radius=36)
-google_checkbox.pack(side="left", padx=(0, 75))
-google_checkbox.select()
-
-pinterest_checkbox = ctk.CTkCheckBox(gp_frame, text="Pinterest", width=50, corner_radius=36)
-pinterest_checkbox.pack(side="right")
-pinterest_checkbox.select()
-
-height_entry = ctk.CTkEntry(res_frame, width=50, placeholder_text="height")
-height_entry.pack(side="left")
-
-width_entry = ctk.CTkEntry(res_frame, width=50, placeholder_text="width")
-width_entry.pack(side="right")
-
-rembg_checkbox = ctk.CTkCheckBox(settingsbar, text="Remove BG", width=50, corner_radius=36)
-rembg_checkbox.grid(row=4, pady=(0, 20))
 
 if __name__ == "__main__":
     window.mainloop()
