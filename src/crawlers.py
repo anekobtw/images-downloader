4830feab6a8f338f18f272642c314296a4dd7579
anekobtw <krolexonder@gmail.com>
2024-10-04 15:26:26 +0400

    changed gui and added pinterest

--- src/crawlers.py
+++ src/crawlers.py
@@ -1,6 +1,8 @@
+import logging
 import os
 from pathlib import Path
 
+import rembg
 from icrawler.builtin import GoogleImageCrawler
 from PIL import Image
 from pinterest_crawler import PinterestCrawler
@@ -22,19 +24,50 @@ def crawl_google(query: str, limit: int = 1000) -> None:
     crawler.crawl(keyword=query, max_num=limit, overwrite=True)
 
 
-def filter_images(height: int, width: int) -> None:
+def filter_images(directory: Path, height: int, width: int) -> None:
     """Filter images based on given resolution."""
-    for file in pinterest_dir.glob("*.*"):
-        img = Image.open(file)
-        if file.suffix not in (".png", ".jpg", ".jpeg") or img.size != (height, width):
+    for file in directory.glob("*.*"):
+        if file.suffix not in (".png", ".jpg", ".jpeg"):
             print(f"Deleting {file.absolute()}")
             os.remove(file)
-        img.close()
+            continue
+
+        img = Image.open(file)
+        if height and width and img.size != (int(height), int(width)):
+            print(f"Deleting {file.absolute()}")
+            img.close()
+            os.remove(file)
+            continue
 
 
-def download_images(query: str) -> None:
-    crawl_pinterest(query, 5)
-    crawl_google(query, 5)
+def remove_background(directory: Path) -> None:
+    """Remove background from the images."""
+    for file in directory.glob("*.*"):
+        logging.debug(f"Removing bg from {file}")
+        input_path = file
+        output_path = file.with_suffix(".png")
+
+        with Image.open(input_path) as img:
+            img = img.convert("RGBA")
+            output = rembg.remove(img)
+            output.save(output_path)
+
+        logging.debug(f"Removed bg from {file}")
+        if input_path != output_path:
+            os.remove(input_path)
+
+
+def download_images(query: str, google: bool, pinterest: bool, height: int, width: int, remove_bg: bool) -> None:
+    if google:
+        crawl_google(query, 150)
+    if pinterest:
+        crawl_pinterest(query)
+
+    filter_images(google_dir, height, width)
+    filter_images(pinterest_dir, height, width)
+    if remove_bg:
+        remove_background(google_dir)
+        remove_background(pinterest_dir)
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 
 import customtkinter as ctk
 
-from src.utils import *
+from crawlers import download_images
 
 # Basic configuring
 __version__ = "1.2.1"
@@ -13,17 +13,17 @@ PADX = 20
 
 
 # App config
-app = ctk.CTk()
-app.title = "Images Downloader"
+window = ctk.CTk()
+window.title = "Images Downloader"
 
-infobar = ctk.CTkFrame(app)
+infobar = ctk.CTkFrame(window)
 infobar.grid(row=0, column=0, padx=PADX, pady=20, sticky="nsew")
 
-settingsbar = ctk.CTkFrame(app)
+settingsbar = ctk.CTkFrame(window)
 settingsbar.grid(row=0, column=1, padx=PADX, pady=20, sticky="ew")
 
-gb_frame = ctk.CTkFrame(settingsbar, fg_color="transparent")
-gb_frame.grid(row=1, pady=10, padx=PADX, sticky="nsew")
+gp_frame = ctk.CTkFrame(settingsbar, fg_color="transparent")
+gp_frame.grid(row=1, pady=10, padx=PADX, sticky="nsew")
 
 res_frame = ctk.CTkFrame(settingsbar, fg_color="transparent")
 res_frame.grid(row=3, pady=10, padx=PADX, sticky="nsew")
@@ -33,7 +33,6 @@ font = ctk.CTkFont(size=20, weight="bold")
 
 # Infobar
 ctk.CTkLabel(infobar, text=f"Images Downloader {__version__}", font=font).grid(padx=PADX, pady=(10, 0))
-
 ctk.CTkLabel(infobar, text="© anekobtw, 2024\ntheme by avalon60").grid(padx=PADX, pady=(5, 10))
 
 query_entry = ctk.CTkEntry(infobar, placeholder_text="Query")
@@ -42,48 +41,38 @@ query_entry.grid(padx=10, pady=(20, 0))
 ctk.CTkButton(
     infobar,
     text="Download",
-    command=lambda: download(
-        query_entry.get(),
-        height_entry.get(),
-        width_entry.get(),
-        rembg_checkbox.get(),
-        google_checkbox.get(),
-        bing_checkbox.get(),
+    command=lambda: download_images(
+        query=query_entry.get(),
+        google=google_checkbox.get(),
+        pinterest=pinterest_checkbox.get(),
+        height=height_entry.get(),
+        width=width_entry.get(),
+        remove_bg=rembg_checkbox.get(),
     ),
 ).grid(padx=10, pady=(10, 20))
 
-ctk.CTkButton(infobar, text="Open Downloads Folder", command=lambda: os.startfile(DOWNLOADS)).grid(pady=(5, 13))
+ctk.CTkButton(infobar, text="Open Downloads Folder", command=lambda: os.startfile("downloads")).grid(pady=(5, 13))
 
 
 # Settings bar
 ctk.CTkLabel(settingsbar, text="Settings", font=font).grid(row=0, pady=(10, 0))
 
-google_checkbox = ctk.CTkCheckBox(gb_frame, text="Google", corner_radius=36)
-google_checkbox.pack(side="left", padx=(0, 100))
+google_checkbox = ctk.CTkCheckBox(gp_frame, text="Google", corner_radius=36)
+google_checkbox.pack(side="left", padx=(0, 75))
 google_checkbox.select()
 
-bing_checkbox = ctk.CTkCheckBox(gb_frame, text="Bing", width=50, corner_radius=36)
-bing_checkbox.pack(side="right")
-bing_checkbox.select()
-
-filter_checkbox = ctk.CTkCheckBox(
-    res_frame,
-    text="Filter",
-    width=50,
-    command=lambda: update_entries(filter_checkbox, height_entry, width_entry),
-)
-filter_checkbox.grid(row=0, column=0, padx=(0, PADX))
+pinterest_checkbox = ctk.CTkCheckBox(gp_frame, text="Pinterest", width=50, corner_radius=36)
+pinterest_checkbox.pack(side="right")
+pinterest_checkbox.select()
 
 height_entry = ctk.CTkEntry(res_frame, width=50, placeholder_text="height")
-height_entry.configure(state="disabled")
-height_entry.grid(row=0, column=1, padx=PADX)
+height_entry.pack(side="left")
 
 width_entry = ctk.CTkEntry(res_frame, width=50, placeholder_text="width")
-width_entry.configure(state="disabled")
-width_entry.grid(row=0, column=2, padx=(PADX, 0))
+width_entry.pack(side="right")
 
 rembg_checkbox = ctk.CTkCheckBox(settingsbar, text="Remove BG", width=50, corner_radius=36)
 rembg_checkbox.grid(row=4, pady=(0, 20))
 
 if __name__ == "__main__":
-    app.mainloop()
+    window.mainloop()
